232a456f6e5986aca3d3081ebc03addbfda8ad10
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-11-27 09:17:22 +0100

    implement `config` component and decouple

--- app/Factory/AppKernelFactory.php
+++ app/Factory/AppKernelFactory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Workspace\Factory;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Waffle\Commons\Container\Container;
+use Waffle\Commons\Config\Config;use Waffle\Commons\Container\Container;
 use Waffle\Commons\Contracts\Core\KernelInterface;
 use Waffle\Commons\Contracts\Http\ResponseEmitterInterface;
 use Waffle\Commons\Http\Emitter\ResponseEmitter;
@@ -22,10 +22,19 @@ final class AppKernelFactory
      */
     public static function create(string $env): KernelInterface
     {
+        /** @var string $root */
+        $root = APP_ROOT;
+        $rootConfig = $root . DIRECTORY_SEPARATOR . APP_CONFIG;
         // 1. Instantiate the concrete Container (from waffle-commons/container)
         $container = new Container();
 
-        // 2. Instantiate the Kernel (from your workspace app)
+        // 2. Instantiate the concrete Config (from waffle-commons/config)
+        $config = new Config(
+            configDir: $rootConfig,
+            environment: $env,
+        );
+
+        // 3. Instantiate the Kernel (from your workspace app)
         // Note: As we migrate, we will inject the container here in the future.
         // For now, we assume your Kernel might instantiate its dependencies internally
         // or accept them via constructor depending on your current Kernel refactoring state.
@@ -34,6 +43,11 @@ final class AppKernelFactory
         // If your Kernel allows injecting the container implementation:
         if (method_exists($kernel, 'setContainerImplementation')) {
             $kernel->setContainerImplementation($container);
+        }
+
+        // If your Kernel allows injecting the container implementation:
+        if (method_exists($kernel, 'setConfiguration')) {
+            $kernel->setConfiguration($config);
         }
 
         return $kernel;
